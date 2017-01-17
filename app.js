51a63c388160b93a17fc2ed82c0dfdb3c2ba400e
Saadna <saadna.ouldzein.daveo@axa.fr>
2017-01-17 16:47:48 +0100

    with push notification

--- app.js
+++ app.js
@@ -1,17 +1,150 @@
 /**
  * Created by saadna on 11/01/2017.
  */
+var swRegistration = null;
+var applicationServerPublicKey = 'BBrm10zUdMNDKHWp5UnCLm0dKbTyhWY9yQuta-ULKIajqc3ndP_Fahdtb3Y_cJtn0pCODpkx2RIBAdH2TvtY8AI';
 
-if ('serviceWorker' in navigator)  {
+var pushButton = document.querySelector('.js-push-btn');
+var isSubscribed = false;
+
+
+
+function urlB64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+function updateBtn() {
+    if (Notification.permission === 'denied') {
+        pushButton.textContent = 'Push Messaging Blocked.';
+        pushButton.disabled = true;
+        updateSubscriptionOnServer(null);
+        return;
+    }
+
+    if (isSubscribed) {
+        pushButton.textContent = 'Disable Push Messaging';
+    } else {
+        pushButton.textContent = 'Enable Push Messaging';
+    }
+
+    pushButton.disabled = false;
+}
+
+
+function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server
+
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+        document.querySelector('.js-subscription-details');
+
+    if (subscription) {
+        subscriptionJson.textContent = JSON.stringify(subscription);
+        subscriptionDetails.classList.remove('is-invisible');
+    } else {
+        subscriptionDetails.classList.add('is-invisible');
+    }
+}
+
+function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+    })
+        .then(function(subscription) {
+            console.log('User is subscribed:', subscription);
+
+            updateSubscriptionOnServer(subscription);
+
+            isSubscribed = true;
+
+            updateBtn();
+        })
+        .catch(function(err) {
+            console.log('Failed to subscribe the user: ', err);
+            updateBtn();
+        });
+}
+
+function unsubscribeUser() {
+    swRegistration.pushManager.getSubscription()
+        .then(function(subscription) {
+            if (subscription) {
+                return subscription.unsubscribe();
+            }
+        })
+        .catch(function(error) {
+            console.log('Error unsubscribing', error);
+        })
+        .then(function() {
+            updateSubscriptionOnServer(null);
+
+            console.log('User is unsubscribed.');
+            isSubscribed = false;
+
+            updateBtn();
+        });
+}
+
+function initialiseUI() {
+    pushButton.addEventListener('click', function() {
+        pushButton.disabled = true;
+        if (isSubscribed) {
+            unsubscribeUser();
+        } else {
+            subscribeUser();
+        }
+    });
+    // Set the initial subscription value
+    swRegistration.pushManager.getSubscription()
+        .then(function(subscription) {
+            isSubscribed = !(subscription === null);
+
+            updateSubscriptionOnServer(subscription);
+
+            if (isSubscribed) {
+               // console.log('User IS subscribed.');
+                unsubscribeUser();
+            } else {
+
+               // console.log('User is NOT subscribed.');
+                subscribeUser();
+            }
+
+            updateBtn();
+        });
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window)  {
+
+    console.log('Service Worker and Push is supported');
 
     navigator.serviceWorker
         .register('./service-worker.js',{scope:'./'})
         .then(function(registration){
              console.log("Service Worker Registered") ;
+            swRegistration = registration;
+            initialiseUI();
         })
         .catch(function(err){
             console.log("Service Worker failed to register",err)
         })
+}
+else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
 }
 
 
@@ -50,6 +183,6 @@ var get = function(url)
     })
     .catch (function(err){
      console.log("Error",err)  ;
-    })
+    });
 
 
--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,10 @@ var urlsToCache = [
         'https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo'
 ];
 
+
+
+
+
 self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
@@ -38,6 +42,8 @@ self.addEventListener('activate', function(event) {
         })
     );
 });
+
+
 
 
 self.addEventListener('fetch', function(event) {
@@ -90,3 +96,61 @@ self.addEventListener('fetch', function(event) {
 });
 
 
+/* eslint-disable max-len */
+
+var applicationServerPublicKey = 'BBrm10zUdMNDKHWp5UnCLm0dKbTyhWY9yQuta-ULKIajqc3ndP_Fahdtb3Y_cJtn0pCODpkx2RIBAdH2TvtY8AI';
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log('[Service Worker] Push had this data:'+ event.data.text());
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+    console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    event.waitUntil(
+        self.registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: applicationServerKey
+        })
+            .then(function(newSubscription) {
+                // TODO: Send to application server
+                console.log('[Service Worker] New subscription: ', newSubscription);
+            })
+    );
+});
